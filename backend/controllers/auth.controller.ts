7bba8351a62ad8580f4bbd5fedc7b16361747bd5
Vishal772-pixel <fantasydark001@gmail.com>
2025-06-03 16:46:26 +0530

    feat: initialize auth controller, routes, services, and JWT utility files

---
+++ backend/controllers/auth.controller.ts
@@ -0,0 +1,8 @@
+import {Request, Response } from "express";
+// import * as AuthService from "../services/auth.services.ts";
+ 
+//  Sign up Logic 
+
+export const register = async (req:Request, res:Response)=>{
+    
+}
